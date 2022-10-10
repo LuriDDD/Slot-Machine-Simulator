1937672a47376c605fe14b04c49f39721ae09278
LuriD <iamyourasistan@gmail.com>
2022-10-11 02:51:04 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -4,23 +4,10 @@
 
 using namespace std;
 
-typedef void (*ptr)(int x, int y);
-
 int main()
 {
-    // SlotMachine slot;
-    // slot.run();
-
-    ptr p;
-    p(1, 1);
-
-    int *a = new int(1);
-    std::vector<int *> v(1);
-    v[0] = a;
-
-    cout << *v[0] << " " << *a;
-    *a = 2;
-    cout << *v[0] << " " << *a;
+    SlotMachine slot;
+    slot.run();
 
     return 0;
 }
